fba0df19a912b8138c1bb3e33cf0363c483b5d42
Stefan Eilemann <eilemann@gmail.com>
2012-07-04 14:14:11 +0200

    Add dependent package versions to version.h, Fix doxygen

--- codash/version.in.h
+++ codash/version.in.h
@@ -28,40 +28,46 @@
 namespace codash
 {
     // CoDASH version macros and functions
-    /** The current major version. @version 0.4 */
+    /** The current major version. @version 0.1 */
 #   define CODASH_VERSION_MAJOR @VERSION_MAJOR@
 
-    /** The current minor version. @version 0.4 */
+    /** The current minor version. @version 0.1 */
 #   define CODASH_VERSION_MINOR @VERSION_MINOR@
 
-    /** The current patch level. @version 0.4 */
+    /** The current patch level. @version 0.1 */
 #   define CODASH_VERSION_PATCH @VERSION_PATCH@
 
-    /** The current DSO binary revision. @version 0.4 */
+    /** The current DSO binary revision. @version 0.1 */
 #   define CODASH_VERSION_ABI @VERSION_ABI@
 
-/** True if the current version is newer than the given one. @version 0.4 */
+    /** The Collage version used to compile codash. @version 0.1 */
+#   define CODASH_COLLAGE_VERSION @COLLAGE_VERSION@
+
+    /** The Dash version used to compile codash. @version 0.1 */
+#   define CODASH_DASH_VERSION @DASH_VERSION@
+
+/** True if the current version is newer than the given one. @version 0.1 */
 #   define CODASH_VERSION_GT( MAJOR, MINOR, PATCH )                   \
     ( (CODASH_VERSION_MAJOR>MAJOR) ||                                 \
       (CODASH_VERSION_MAJOR==MAJOR &&                                 \
        (CODASH_VERSION_MINOR>MINOR ||                                 \
         (CODASH_VERSION_MINOR==MINOR && CODASH_VERSION_PATCH>PATCH))))
 
-/** True if the current version is equal or newer to the given. @version 0.4 */
+/** True if the current version is equal or newer to the given. @version 0.1 */
 #   define CODASH_VERSION_GE( MAJOR, MINOR, PATCH )                         \
     ( (CODASH_VERSION_MAJOR>MAJOR) ||                                       \
       (CODASH_VERSION_MAJOR==MAJOR &&                                 \
        (CODASH_VERSION_MINOR>MINOR ||                                 \
         (CODASH_VERSION_MINOR==MINOR && CODASH_VERSION_PATCH>=PATCH))))
 
-/** True if the current version is older than the given one. @version 0.4 */
+/** True if the current version is older than the given one. @version 0.1 */
 #   define CODASH_VERSION_LT( MAJOR, MINOR, PATCH )                         \
     ( (CODASH_VERSION_MAJOR<MAJOR) ||                                       \
       (CODASH_VERSION_MAJOR==MAJOR &&                                 \
        (CODASH_VERSION_MINOR<MINOR ||                                 \
         (CODASH_VERSION_MINOR==MINOR && CODASH_VERSION_PATCH<PATCH))))
 
-/** True if the current version is older or equal to the given. @version 0.4 */
+/** True if the current version is older or equal to the given. @version 0.1 */
 #   define CODASH_VERSION_LE( MAJOR, MINOR, PATCH )                         \
     ( (CODASH_VERSION_MAJOR<MAJOR) ||                                       \
       (CODASH_VERSION_MAJOR==MAJOR &&                                 \
@@ -72,25 +78,25 @@ namespace codash
     class CODASH_API Version
     {
     public:
-        /** @return the current major version of CoDASH. @version 0.4 */
+        /** @return the current major version of CoDASH. @version 0.1 */
         static uint32_t getMajor();
 
-        /** @return the current minor version of CoDASH. @version 0.4 */
+        /** @return the current minor version of CoDASH. @version 0.1 */
         static uint32_t getMinor();
 
-        /** @return the current patch level of CoDASH. @version 0.4 */
+        /** @return the current patch level of CoDASH. @version 0.1 */
         static uint32_t getPatch();
 
-        /** @return the current revision of CoDASH. @version 0.4 */
+        /** @return the current revision of CoDASH. @version 0.1 */
         static std::string getRevision();
 
-        /** @return the current DSO binary revision. @version 0.4 */
+        /** @return the current DSO binary revision. @version 0.1 */
         static uint32_t getABI();
 
-        /** @return the current CoDASH version (MMmmpp). @version 0.4 */
+        /** @return the current CoDASH version (MMmmpp). @version 0.1 */
         static uint32_t getInt();
 
-        /** @return the current CoDASH version (MM.mmpp). @version 0.4 */
+        /** @return the current CoDASH version (MM.mmpp). @version 0.1 */
         static float    getFloat();
 
         /**
