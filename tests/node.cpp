f851f320b83352c90bb7c44a9fbaef7ad4a65153
Juan Hernando <jhernando@fi.upm.es>
2014-04-09 17:44:05 +0200

    Project related improvements. Updated CMake scripts. Use autogenerated files and removed unnecessary files. Compilation fixes for latest Lunchbox.

--- tests/node.cpp
+++ tests/node.cpp
@@ -1,5 +1,5 @@
 
-/* Copyright (c) 2012, EPFL/Blue Brain Project
+/* Copyright (c) 2012-2014, EPFL/Blue Brain Project
  *                     Daniel Nachbaur <daniel.nachbaur@epfl.ch>
  *
  * This file is part of CoDASH <https://github.com/BlueBrain/codash>
@@ -7,12 +7,12 @@
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License version 3.0 as published
  * by the Free Software Foundation.
- *  
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -24,8 +24,7 @@
 
 #include <codash/distributable.h>
 #include <co/co.h>
-#include <dash/dash.h>
-
+#include <lunchbox/lunchbox.h>
 
 typedef codash::Distributable< dash::Node > NodeDist;
 
