1427e496d90d1759b4916a7b7d0474af46aa2f61
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2014-04-14 11:56:07 +0200

    Update CMake subtree and generated files

--- CMake/codash.cmake
+++ CMake/codash.cmake
@@ -1,12 +1,12 @@
 
 
-set(CODASH_PACKAGE_VERSION 0.1.0)
+set(CODASH_PACKAGE_VERSION 0.1)
 set(CODASH_DEPENDS bluebrain REQUIRED dash Collage Boost)
 set(CODASH_REPO_URL https://github.com/BlueBrain/codash.git)
 set(CODASH_REPO_TAG master)
 set(CODASH_BOOST_COMPONENTS "program_options serialization system thread date_time")
 set(CODASH_DEB_DEPENDS libboost-program-options-dev libboost-serialization-dev
-  libboost-date-time-dev libboost-system-dev libboost-thread-dev)
+libboost-thread-dev libboost-system-dev libboost-date-time-dev)
 set(CODASH_SOURCE "${CMAKE_SOURCE_DIR}/src/codash")
 set(CODASH_FORCE_BUILD ${CI_BUILD})
 
